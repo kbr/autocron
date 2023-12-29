42ea1e3e03ab62761f29233cefc4407dbade967f
Klaus Bremer <bremer@bremer-media.com>
2023-12-29 12:12:15 +0100

    stop() funtion added to the __init__.py module.
    database_file argument to start() is now required. Adapted documentation.

--- autocron/__init__.py
+++ autocron/__init__.py
@@ -1,7 +1,8 @@
 """
 autocron:
 
-simple background task handling with no dependencies.
+simple asynchronous background task handling with no dependencies beside
+the standard-library.
 """
 from .decorators import (
     cron,
@@ -10,15 +11,28 @@ from .decorators import (
 from .engine import Engine
 
 
-__all__ = ["cron", "delay", "start"]
-__version__ = "0.7.dev"
+__all__ = ["cron", "delay", "start", "stop"]
+__version__ = "0.7.1.dev"
 
 _engine = Engine()
 
 
-def start(database_file=None):
+def start(database_file):
     """
-    Call this from the framework of choice to explicitly
-    activate autocron.
+    Call this from the framework of choice to explicitly activate
+    autocron. ``database_file`` is a string with the file-name of the
+    database. The file gets stored in the ``~.autocron/`` directory. If
+    this directory does not exist, it will get created. The file-name can
+    also be an absolute path so the file will get stored elsewere. In
+    this case all directories in the path must exist.
     """
     _engine.start(database_file=database_file)
+
+
+def stop():
+    """
+    Stops autocron explicitly. On receiving a termination-signal
+    autocron invokes a shutdown sequence to stop the workers, so calling
+    ``stop`` is normalwise not required.
+    """
+    _engine.stop()
