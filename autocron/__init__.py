1c6e2c41ce2d32676752bb59e4016810021180b1
Klaus Bremer <bremer@bremer-media.com>
2024-03-11 10:08:12 +0100

    set version to 0.9.3

--- autocron/__init__.py
+++ autocron/__init__.py
@@ -13,7 +13,7 @@ from .sql_interface import SQLiteInterface
 
 
 __all__ = ["cron", "delay", "start", "stop", "get_results"]
-__version__ = "0.9.2"
+__version__ = "0.9.3"
 
 _engine = Engine()
 _interface = SQLiteInterface()
