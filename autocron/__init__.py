60d17dd55bdba562de15fe2633b28108d0ca4b2a
Klaus Bremer <bremer@bremer-media.com>
2024-08-16 15:20:54 +0200

    fix readthedocs changes

--- autocron/__init__.py
+++ autocron/__init__.py
@@ -20,7 +20,7 @@ from autocron.sqlite_interface import SQLiteInterface
 
 
 __all__ = ["cron", "delay", "start", "stop"]
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 _engine = Engine()
 _interface = SQLiteInterface()
