b38e82005d1a3d1b387ba2eec64563baf7f373c2
Klaus Bremer <bremer@bremer-media.com>
2024-10-03 16:25:43 +0200

    set version to 1.2.0

--- autocron/__init__.py
+++ autocron/__init__.py
@@ -20,7 +20,7 @@ from autocron.sqlite_interface import SQLiteInterface
 
 
 __all__ = ["cron", "delay", "start", "stop"]
-__version__ = "1.2.0 dev"
+__version__ = "1.2.0"
 
 _engine = Engine()
 _interface = SQLiteInterface()
